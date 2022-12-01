b3aedcca4854111faa78471cb39b0e771e2a9776
BlackWhite-code <3331619771@qq.com>
2022-12-01 23:15:18 +0800

    twoSum.c

--- twoSum.c
+++ twoSum.c
@@ -1,10 +1,12 @@
+// 在一个数组中找到两个数字相加得到target的值，然后返回下标
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
 	for (int i = 0; i < numsSize; ++i){
 		for(in j = i+1; j < numsSize; ++j){
 			if(nums[i] + nums[j] == target){
 				int* ret = malloc(sizeof(int)*2);
-                        	ret[0] = i;
+                ret[0] = i;
 				ret[1] = j;
 				*returnSize = 2;
 				return ret;
